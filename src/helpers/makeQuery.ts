6c0ea3f53f726286d39cd0cb64826bf5f0836744
p34-pac <peterpaultolulope@gmail.com>
2025-02-08 21:26:06 +0100

    Update makeQuery function to use the new API endpoint

--- src/helpers/makeQuery.ts
+++ src/helpers/makeQuery.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export async function makeQuery(endpoint: string) {
-  const res = await axios.get(`${endpoint}`)
+  const res = await axios.get(`https://api-anikii.onrender.com${endpoint}`)
 
   return res
 }
